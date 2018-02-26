46b932c4ddc5b2247a9f9a4710977a40fcaeaa44
nongguitao <nongguitao@qgutech.com>
2018-02-26 10:16:54 +0800

    copy to index

--- build/file.js
+++ build/file.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 /** */
-exports.getAllFiles = dir => {
+const getAllFiles = srcdir => {
     let filesArr = [];
-    (function readFiles(dir) {
+    const readFiles = dir => {
         let files = fs.readdirSync(dir);
 
         if (files.length) {
@@ -18,7 +18,49 @@ exports.getAllFiles = dir => {
                 }
             });
         }
-    })(dir);
+    };
+    readFiles(srcdir);
 
     return filesArr;
+};
+
+const copyFolder = (srcdir, tardir, cb) => {
+    fs.readdir(srcdir, (err, files) => {
+        let count = 0;
+        let checkEnd = () => {
+            ++count == files.length && cb && cb();
+        };
+
+        if (err) {
+            checkEnd();
+            return;
+        }
+
+        files.forEach(file => {
+            console.log(file);
+            let srcPath = path.join(srcdir, file);
+            let tarPath = path.join(tardir, file);
+
+            fs.stat(srcPath, (err, stats) => {
+                if (stats.isDirectory()) {
+                    fs.mkdir(tarPath, err => {
+                        if (err) {
+                            console.log(err);
+                        }
+                        copyFolder(srcPath, tarPath, checkEnd);
+                    });
+                } else {
+                    fs.copyFileSync(srcPath, tarPath, checkEnd);
+                }
+            });
+        });
+
+        //为空时直接回调
+        files.length === 0 && cb && cb();
+    });
+};
+
+module.exports = {
+    copyFolder: copyFolder,
+    getAllFiles: getAllFiles
 };
