0efebcaf5a400bb695644d915a9033b427f3c815
ngt <inoongt@gmail.com>
2018-05-06 14:58:11 +0800

    update

--- static/pages/App.js
+++ static/pages/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import { renderRoutes } from "react-router-config";
 import Header from "../components/Header";
 
